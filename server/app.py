611edc81e5cf2b2213beef1e15259e2c5a37a799
brewchetta <chett.tiller@gmail.com>
2023-09-15 14:30:14 -0400

    scientists! wowzers!

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from models import db, Scientist, Mission, Planet
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
+import sqlalchemy
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -25,6 +26,9 @@ db.init_app(app)
 def home():
     return ''
 
+
+# ---PLANETS--- #
+
 # GET /planets
 @app.get('/planets')
 def all_planets():
@@ -40,6 +44,9 @@ def planet_by_id(id):
         return jsonify( planet.to_dict() ), 200
     except AttributeError:
         return jsonify( {"error": "No planet found"} ), 404
+
+
+# ---SCIENTISTS--- #
 
 # GET /scientists
 @app.get('/scientists')
@@ -63,6 +70,7 @@ def create_scientist():
     data = request.json
     print(data)
     try:
+        # scientist = Scientist(**data)
         scientist = Scientist(name=data["name"], field_of_study=data["field_of_study"])
         db.session.add( scientist )
         db.session.commit()
@@ -72,10 +80,44 @@ def create_scientist():
 
 
 # PATCH /scientists/:id
+@app.patch('/scientists/<int:id>')
+def update_scientist(id):
+    try:
+        data = request.json
+        Scientist.query.filter( Scientist.id == id ).update( data )
+        db.session.commit()
+
+        scientist = Scientist.query.filter(Scientist.id == id).first()
+        return jsonify( scientist.to_dict() ), 202
+    except sqlalchemy.exc.IntegrityError as e:
+        return jsonify( {"error": "Invalid data", "error_message": str(e)} ), 406
+
 
 # DELETE /scientists/:id
+@app.delete('/scientists/<int:id>')
+def destroy_scientist(id):
+    try:
+        scientist = Scientist.query.filter(Scientist.id == id).first()
+        db.session.delete( scientist )
+        db.session.commit()
+        return {}, 204
+    except sqlalchemy.orm.exc.UnmappedInstanceError:
+        return jsonify( {"error": "Not found" } ), 404
+
+
+# ---MISSIONS--- #
 
 # POST /missions
+@app.post('/missions')
+def create_mission():
+    data = request.json
+    planet = Planet.query.filter(Planet.id == data["planet_id"]).first()
+    scientist = Scientist.query.filter(Scientist.id == data["scientist_id"]).first()
+    mission = Mission(name=data["name"], scientist=scientist, planet=planet)
+    db.session.add(mission)
+    db.session.commit()
+
+    return jsonify( mission.to_dict() ), 201
 
 
 if __name__ == '__main__':
